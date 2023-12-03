cd1c46fe8f09f3d09274f88f3f84c2b7f24282b2
Lysander <lysander6@gmail.com>
2023-12-03 16:59:44 +0100

    Day 3 part 1 solution

---
+++ day_03/src/lib.rs
@@ -0,0 +1,172 @@
+use std::{collections::HashMap, str::FromStr};
+
+#[derive(Debug, Default, PartialEq, Eq)]
+pub struct Problem {
+    numbers: Vec<((usize, usize), u32, usize)>,
+    symbols: HashMap<(usize, usize), char>,
+}
+
+impl FromStr for Problem {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut p = Problem::default();
+
+        for (x, line) in s.lines().enumerate() {
+            let number_slices = find_number_slices(line);
+            let numbers = number_slices
+                .iter()
+                .map(|(y, s)| ((x, *y), s.parse::<u32>().unwrap(), s.len()));
+
+            p.numbers.extend(numbers);
+
+            let symbols = line.char_indices().filter_map(|(y, c)| {
+                if c != '.' && !c.is_ascii_digit() {
+                    Some(((x, y), c))
+                } else {
+                    None
+                }
+            });
+
+            p.symbols.extend(symbols);
+        }
+
+        Ok(p)
+    }
+}
+
+fn find_number_slices(s: &str) -> Vec<(usize, String)> {
+    let mut i = 0;
+    let mut v = Vec::new();
+
+    while let Some(offset) = s[i..].find(|c: char| c.is_ascii_digit()) {
+        let idx = i + offset;
+        let j = s[idx..].find(|c: char| !c.is_ascii_digit());
+
+        if let Some(j) = j {
+            let idx_end = idx + j;
+            v.push((idx, s[idx..idx_end].to_string()));
+            i = idx_end;
+        } else {
+            v.push((idx, s[idx..].to_string()));
+            break;
+        }
+    }
+
+    v
+}
+
+fn neighbour_offsets(i: usize, j: usize, max_i: usize, max_j: usize) -> &'static [(isize, isize)] {
+    match (i, j) {
+        (0, 0) => &[(0, 1), (1, 0), (1, 1)],
+        (0, j) if j == max_j => &[(0, -1), (1, 0), (1, -1)],
+        (0, _) => &[(0, -1), (0, 1), (1, -1), (1, 0), (1, 1)],
+        (i, 0) if i == max_i => &[(-1, 0), (-1, 1), (0, 1)],
+        (_, 0) => &[(-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)],
+        (i, j) if i == max_i && j == max_j => &[(0, -1), (-1, 0), (-1, -1)],
+        (i, _) if i == max_i => &[(0, -1), (0, 1), (-1, -1), (-1, 0), (-1, 1)],
+        (_, j) if j == max_j => &[(0, -1), (-1, 0), (1, 0), (-1, -1), (1, -1)],
+        (_, _) => &[
+            (-1, 0),
+            (1, 0),
+            (0, -1),
+            (0, 1),
+            (-1, -1),
+            (-1, 1),
+            (1, 1),
+            (1, -1),
+        ],
+    }
+}
+
+#[must_use]
+pub fn solve_part_1(p: &Problem) -> u32 {
+    let Problem { numbers, symbols } = p;
+
+    numbers
+        .iter()
+        .filter_map(|&((x, y), number, len)| {
+            if (y..(y + len))
+                .flat_map(|y| {
+                    neighbour_offsets(x, y, usize::MAX, usize::MAX)
+                        .iter()
+                        .map(move |&(dx, dy)| {
+                            ((x as isize + dx) as usize, (y as isize + dy) as usize)
+                        })
+                })
+                .any(|k| symbols.contains_key(&k))
+            {
+                Some(number)
+            } else {
+                None
+            }
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = "\
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+
+    #[test]
+    fn test_find_number_slices() {
+        let s = "543.*...&231..123";
+
+        assert_eq!(
+            find_number_slices(s),
+            vec![
+                (0, "543".to_string()),
+                (9, "231".to_string()),
+                (14, "123".to_string())
+            ]
+        );
+    }
+
+    #[test]
+    fn test_problem_parsing() {
+        let p: Problem = TEST_INPUT.parse().unwrap();
+        assert_eq!(
+            p,
+            Problem {
+                numbers: vec![
+                    ((0, 0), 467, 3),
+                    ((0, 5), 114, 3),
+                    ((2, 2), 35, 2),
+                    ((2, 6), 633, 3),
+                    ((4, 0), 617, 3),
+                    ((5, 7), 58, 2),
+                    ((6, 2), 592, 3),
+                    ((7, 6), 755, 3),
+                    ((9, 1), 664, 3),
+                    ((9, 5), 598, 3)
+                ],
+                symbols: HashMap::from([
+                    ((8, 5), '*'),
+                    ((1, 3), '*'),
+                    ((3, 6), '#'),
+                    ((4, 3), '*'),
+                    ((5, 5), '+'),
+                    ((8, 3), '$')
+                ])
+            }
+        );
+    }
+
+    #[test]
+    fn test_solve_part_1() {
+        let p: Problem = TEST_INPUT.parse().unwrap();
+        assert_eq!(solve_part_1(&p), 4361);
+    }
+}
